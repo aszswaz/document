d5128a4c75d6fb4d115a7df3c3f363201c562af8
朽木不可雕也 <aszswaz@163.com>
2021-06-16 21:10:52 +0800

    backup

--- scripts/console/elasticsearch.sh
+++ scripts/console/elasticsearch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # 获取elasticsearch服务器的所有索引
-get_elasticsearch_indies(){
-    elasticsearch '_aliases' | jq keys
+get_elasticsearch_indies() {
+  elasticsearch '_aliases' | jq keys
 }
 
 # 连接elasticsearch服务器，并且根据指定的参数，执行相应的查询操作
-elasticsearch(){
-   curl -X GET -v "http://${ELASTICSEARCH_SERVER}:${ELASTICSEARCH_PORT}/$1" --USER ${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD} | jq
+elasticsearch() {
+  curl -X GET -v "http://${ELASTICSEARCH_SERVER}:${ELASTICSEARCH_PORT}/$1" --USER "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" | jq
 }
--- scripts/console/git-comments.sh
+++ scripts/console/git-comments.sh
@@ -1,19 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 # git的自定义指令
 
-git_push_all(){
-    if git rev-parse; then
-	    result=($(git remote))
-        printf "检测到${#result}个远程仓库。\n"
-        # 将代码提交到git的所有仓库
-        for depository in $(git remote)
-        do
-            printf "正在提交: ${depository}\n"
-            git push ${depository}
-            git push --tags ${depository}
-        done
-        echo "完毕!"
-    else
-        echo "$(pwd)：不是git仓库！"
-    fi
+git_push_all() {
+  if git rev-parse; then
+    array=()
+    # shellcheck disable=SC2030
+    git remote | while IFS="" read -r line; do array+=("${line}"); done
+    # shellcheck disable=SC2031
+    echo "检测到${#array}个远程仓库。"
+    # 将代码提交到git的所有仓库
+    # shellcheck disable=SC2031
+    for depository in "${array[@]}"; do
+      echo "正在提交: ${depository}"
+      git push "${depository}"
+      git push --tags "${depository}"
+    done
+    echo "完毕!"
+  else
+    echo "$(pwd)：不是git仓库！"
+  fi
 }
