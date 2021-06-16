4d533f9941c5f209ef7190423650b2083e4bc431
朽木不可雕也 <aszswaz@163.com>
2021-06-16 21:12:24 +0800

    backup

--- scripts/console/v2ray.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker_run_v2ray(){
-    sudo docker container stop $1
-    sudo docker container rm $1
-    sudo docker run -d -t --name=$1 -m=124M --memory-swap=-1 -v /etc/v2ray:/etc/v2ray -v /etc/hosts:/etc/hosts -p $2:$2 v2ray/official  v2ray -config=/etc/v2ray/$1.json
-}
