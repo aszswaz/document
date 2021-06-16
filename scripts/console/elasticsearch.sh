b1fbdf709f0de9d6df27f5004d5e034a10b2628d
朽木不可雕也 <aszswaz@163.com>
2021-06-16 23:40:09 +0800

    backup

--- scripts/console/elasticsearch.sh
+++ scripts/console/elasticsearch.sh
@@ -10,24 +10,25 @@ elasticsearch() {
   if [ "$1" = "search" ] || [ "$1" = "count" ]; then
     if [ "$#" -lt "2" ]; then
       echo "请输入索引名称" 1>&2
-      return 0
+      return 1
     fi
 
     # 读取es的查询条件文件
-    query_file='query.json'
     if [ "$#" -ge "3" ]; then
       query_file="$3"
-    fi
-
-    if [ ! -r "${query_file}" ]; then
-      echo "文件：${query_file}不存在，或者无法读取" 1>&2
+      if [ ! -r "${query_file}" ]; then
+        echo "文件：${query_file}不存在，或者无法读取" 1>&2
+        return 1
+      fi
+      curl -X GET -s \
+        "http://${ELASTICSEARCH_SERVER}:${ELASTICSEARCH_PORT}/$2/_$1" --USER "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" \
+        -d "@${query_file}" \
+        -H 'Content-Type: application/json' |
+        jq
       return 0
     fi
-
     curl -X GET -s \
-      "http://${ELASTICSEARCH_SERVER}:${ELASTICSEARCH_PORT}/$2/_$1" --USER "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" \
-      -d "@${query_file}" \
-      -H 'Content-Type: application/json' |
+      "http://${ELASTICSEARCH_SERVER}:${ELASTICSEARCH_PORT}/$2/_$1" --USER "${ELASTICSEARCH_USERNAME}:${ELASTICSEARCH_PASSWORD}" |
       jq
     return 0
   fi
@@ -40,51 +41,58 @@ get_elasticsearch_indies() {
   array=()
   elasticsearch '_aliases' | jq keys |
     while read -r line; do
-      # shellcheck disable=SC2039
       line="${line/\[/}"
-      # shellcheck disable=SC2039
       line="${line/\"/}"
-      # shellcheck disable=SC2039
       line="${line/,/}"
-      # shellcheck disable=SC2039
       line="${line/\"/}"
-      # shellcheck disable=SC2039
       line="${line/\]/}"
-      # shellcheck disable=SC2030
       array+=("${line}")
     done
-  # shellcheck disable=SC2068
-  # shellcheck disable=SC2068
-  # shellcheck disable=SC2031
   for index in ${array[@]}; do
     echo "$index"
   done
 }
 
+# 按照查询条件，对索引进行聚合查询，默认查询全部的索引
 elasticsearch_count() {
   count_number=0
   get_elasticsearch_indies |
     while read -r line; do
       if [ "$#" = "0" ]; then
         request=$(elasticsearch count "$line" | jq .count)
-        # shellcheck disable=SC2046
-        # shellcheck disable=SC2003
-        # shellcheck disable=SC2030
+
+        if [ "$request" = "" ]; then
+          return 1
+        fi
+
         count_number=$(expr "${count_number}" + "${request}")
         echo "index: ${line}, count: ${request}"
         continue
       fi
-      # shellcheck disable=SC2076
-      if [[ "$line" =~ "$1" ]]; then
+
+      if [ "$#" -eq "1" ] && [[ "$line" =~ "$1" ]]; then
         request=$(elasticsearch count "$line" | jq .count)
-        # shellcheck disable=SC2046
-        # shellcheck disable=SC2003
-        # shellcheck disable=SC2030
+
+        if [ "$request" = "" ]; then
+          return 1
+        fi
+
+        count_number=$(expr "${count_number}" + "${request}")
+        echo "index: ${line}, count: ${request}"
+        continue
+      fi
+
+      if [ "$#" -eq "2" ] && [[ "$line" =~ "$1" ]]; then
+        request=$(elasticsearch count "$line" "$2" | jq .count)
+
+        if [ "$request" = "" ]; then
+          return 1
+        fi
+
         count_number=$(expr "${count_number}" + "${request}")
         echo "index: ${line}, count: ${request}"
         continue
       fi
     done
-  # shellcheck disable=SC2031
   echo "total count: ${count_number}"
 }
